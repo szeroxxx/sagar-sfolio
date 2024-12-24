bc754bf440d2b17df99e96198b6323a2c4db8fdb
szeroxxx <szeroxxx0@gmail.com>
2024-12-24 17:11:02 +0530

    spinner

--- components/WorkExperience.js
+++ components/WorkExperience.js
@@ -26,12 +26,10 @@ const JobCard = ({ company, year, description, tools }) => (
         {description}
       </p>
 
-      <div
-        className="flex flex-wrap gap-2 mb-4"
-      >
+      <div className="flex flex-wrap gap-2 mb-4">
         {tools.map((tool, index) => (
-          <span 
-            key={index} 
+          <span
+            key={index}
             className="px-3 py-1 text-xs sm:text-sm md:text-base bg-gray-700 text-gray-300 rounded-full 
             shadow-md hover:bg-gray-600 hover:text-white transform transition-all duration-300 hover:translate-x-2"
           >
@@ -43,17 +41,27 @@ const JobCard = ({ company, year, description, tools }) => (
   </div>
 );
 
+const LoadingSpinner = () => (
+  <div className="flex justify-center items-center h-40">
+    <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-b-4 border-blue-500">
+      <div className="h-16 w-16 rounded-full border-t-4 border-b-4 border-blue-300 animate-ping"></div>
+    </div>
+  </div>
+);
 
 export default function WorkExperience() {
   const [exp, setExp] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   const fetchExperience = async () => {
     try {
       const response = await fetch("/api/experience");
       const data = await response.json();
       setExp(data);
+      setLoading(false);
     } catch (error) {
       console.error("Error fetching data:", error);
+      setLoading(false);
     }
   };
 
@@ -64,9 +72,13 @@ export default function WorkExperience() {
   return (
     <section className="mb-16">
       <h2 className="text-2xl font-bold mb-4">Work Experience 📚</h2>
-      {exp.map((job, index) => (
-        <JobCard key={index} {...job} />
-      ))}
+      {loading ? (
+        <LoadingSpinner />
+      ) : (
+        exp.map((job, index) => (
+          <JobCard key={index} {...job} />
+        ))
+      )}
     </section>
   );
 }
